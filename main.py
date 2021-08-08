ef665f9e5177c3cba1c9bca142145fd1228c2659
Marcos Soares <marcos.fsoares@yahoo.com>
2021-08-08 19:20:59 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2 as cv  # pacote opencv-python
+# opencv depende também do pacote numpy
+
+
+webcam = cv.VideoCapture(0)
+
+if webcam.isOpened():
+    print("Webcam conectada.")
+    validacao, frame = webcam.read()
+
+    while validacao:
+        validacao, frame = webcam.read()
+        cv.imshow("Captura de Video da Vida", frame)
+        key = cv.waitKey(16)
+        if key == 27:  # ESC
+            break
+    cv.imwrite("FotoMarcos.png", frame)
+
+webcam.release()
+cv.destroyAllWindows()
